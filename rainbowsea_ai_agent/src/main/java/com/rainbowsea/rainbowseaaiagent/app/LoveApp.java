0132e76096ea8dbb28bc2e8ed7eeee901875f402
RainbowSea <2969377330@qq.com>
2025-08-11 17:55:20 +0800

    - RAG 实战: Spring AI + 本地知识库 - RAG 实战: Spring AI + 云知识库服务 - RAG最佳实践和调优: 优化原始文档,文档切片,元数据标注,向量转换和存储,文档过滤和检索,查询增强和关联 - Spring AI 工具调用(Tool Calling) 实战使用

--- rainbowsea_ai_agent/src/main/java/com/rainbowsea/rainbowseaaiagent/app/LoveApp.java
+++ rainbowsea_ai_agent/src/main/java/com/rainbowsea/rainbowseaaiagent/app/LoveApp.java
@@ -2,17 +2,26 @@ package com.rainbowsea.rainbowseaaiagent.app;
 
 import com.rainbowsea.rainbowseaaiagent.advisor.MyLoggerAdvisor;
 import com.rainbowsea.rainbowseaaiagent.advisor.ReReadingAdvisor;
+import com.rainbowsea.rainbowseaaiagent.rag.LoveAppRagCustomAdvisorFactory;
+import com.rainbowsea.rainbowseaaiagent.rag.QueryRewriter;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 //import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+
 
 //import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 
@@ -20,7 +29,6 @@ import reactor.core.publisher.Flux;
 @Component
 public class LoveApp {
 
-    private final ChatClient chatClient;
 
     private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
             "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
@@ -49,7 +57,7 @@ public class LoveApp {
                         // 自定义日志 Advisor，可按需开启
                         new MyLoggerAdvisor()
 //                        // 自定义推理增强 Advisor，可按需开启
-                       ,new ReReadingAdvisor()
+                        , new ReReadingAdvisor()
                 )
                 .build();
     }
@@ -129,4 +137,115 @@ public class LoveApp {
     //            )
     //            .build();
     //}
+
+
+    // AI 恋爱知识库问答功能
+
+    @Resource
+    private VectorStore loveAppVectorStore;
+
+    @Resource
+    private Advisor loveAppRagCloudAdvisor;
+
+
+    @Resource
+    private QueryRewriter queryRewriter;
+
+    private final ChatClient chatClient;
+
+
+    /**
+     * 和 RAG 知识库进行对话
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChatWithRag(String message, String chatId) {
+        String rewrittenMessage = queryRewriter.doQueryRewrite(message);
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                // 使用改写后的查询
+                .user(rewrittenMessage)
+                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, chatId))
+                // 开启日志，便于观察效果
+                .advisors(new MyLoggerAdvisor())
+                // 应用 RAG 检索增强服务（基于云知识库服务）
+                //.advisors(loveAppRagCloudAdvisor)
+                // 应用 RAG 知识库问答
+                //.advisors(new QuestionAnswerAdvisor(loveAppVectorStore))
+                // 应用自定义的 RAG 检索增强服务（文档查询器 + 上下文增强器）
+                .advisors(
+                        LoveAppRagCustomAdvisorFactory.createLoveAppRagCustomAdvisor(
+                                loveAppVectorStore, "单身"
+                        )
+                )
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
+
+
+
+
+
+    // AI 调用工具能力
+    @Resource
+    private ToolCallback[] allTools;
+
+    /**
+     * AI 恋爱报告功能（支持调用工具）
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChatWithTools(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, chatId))
+                // 开启日志，便于观察效果
+                .advisors(new MyLoggerAdvisor())
+                .toolCallbacks(allTools)
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
+
+
+
+//    public String doChatWithRag(String message, String chatId) {
+//
+//        ChatResponse chatResponse = chatClient
+//                .prompt()
+//                // 使用改写后的查询
+//                .user(message)
+//                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, chatId))
+//                // 开启日志，便于观察效果
+//                .advisors(new MyLoggerAdvisor())
+//                // 应用 RAG 知识库问答
+//                .advisors(new QuestionAnswerAdvisor(loveAppVectorStore))
+//                // 应用 RAG 检索增强服务（基于云知识库服务）
+////                .advisors(loveAppRagCloudAdvisor)
+//                // 应用 RAG 检索增强服务（基于 PgVector 向量存储）
+////                .advisors(new QuestionAnswerAdvisor(pgVectorVectorStore))
+//                // 应用自定义的 RAG 检索增强服务（文档查询器 + 上下文增强器）
+////                .advisors(
+////                        LoveAppRagCustoFmAdvisorFactory.createLoveAppRagCustomAdvisor(
+////                                loveAppVectorStore, "单身"
+////                        )
+////                )
+//                .call()
+//                .chatResponse();
+//        String content = chatResponse.getResult().getOutput().getText();
+//        log.info("content: {}", content);
+//        return content;
+//    }
 }
